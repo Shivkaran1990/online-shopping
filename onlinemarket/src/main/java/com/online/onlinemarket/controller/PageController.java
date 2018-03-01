efc48a5bbd55ab83dbde851744f604f133b55188
Shivkaran Ravidas <s.ravidas01@gmail.com>
2018-03-01 22:57:58 +0530

    menu tab are working now

--- onlinemarket/src/main/java/com/online/onlinemarket/controller/PageController.java
+++ onlinemarket/src/main/java/com/online/onlinemarket/controller/PageController.java
@@ -9,20 +9,30 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
-	@RequestMapping(value={"/","/home","/index"})
+	@RequestMapping(value={"/","/homepage","/index"})
 	public ModelAndView index()
 	{
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","shivkaran ravidas new site");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
 	
-	@RequestMapping(value={"/test"}) 
-	public ModelAndView test(@RequestParam("greeting")String greeting)
+	@RequestMapping(value={"/about"})
+	public ModelAndView about()
 	{
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting",greeting);
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
 		return mv;
 	}
-
+	@RequestMapping(value={"/contact"})
+	public ModelAndView contact()
+	{
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+		return mv;
+	}
+	
 }
---
+++ onlinemarket/src/main/webapp/WEB-INF/views/comman/footer.jsp
@@ -0,0 +1,6 @@
+ <footer class="footer bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright & Shivkaran Ravidas OnlineMarket.com 2018</p>
+      </div>
+      <!-- /.container py-5 bg-dark -->
+ </footer>
